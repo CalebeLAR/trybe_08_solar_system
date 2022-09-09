ef02a0fa33fbba37c7664e23a784b93d7491511e
CalebeLAR <ca6ele@gmail.com>
2022-09-09 10:29:25 -0400

    lint

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        {' '}
+        {/* pesquisar como rederizar uma imgem de fundo para a tag header. */}
+        <h1>Sistema Solar</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
