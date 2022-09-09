d992f9a87b606da605009a3dea43a549f2da5470
CalebeLAR <ca6ele@gmail.com>
2022-09-09 10:34:02 -0400

    lint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
+import Title from './components/Title';
 
 class App extends React.Component {
   render() {
+    const headLine = 'texto recebido como props para o componente Title';
     return (
       <>
         <Header />
         <SolarSystem />
+        <Title headline={ headLine } />
       </>
     );
   }
